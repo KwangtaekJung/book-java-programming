66fc7167419f957b69118883142d2a9a37b48c5c
KwangtaekJung <kt79.jung@gmail.com>
2023-09-03 19:00:39 +0900

    ch07.part05 Generics wildcard

---
+++ src/main/java/ch07/part05/generics/wildcard/TypeA.java
@@ -0,0 +1,4 @@
+package ch07.part05.generics.wildcard;
+
+public class TypeA<T> {
+}
---
+++ src/main/java/ch07/part05/generics/wildcard/TypeE.java
@@ -0,0 +1,8 @@
+package ch07.part05.generics.wildcard;
+
+/**
+ * U 가 정의되어 있지 않기 때무에 오류 발생
+ * @param <T>
+ */
+//public class TypeE<T extends TypeA<U>> {
+//}
